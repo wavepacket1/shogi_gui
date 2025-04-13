7f9200724c390232d649559834e95f56d8f8a42c
wavepacket1 <Mr.aizakku3@gmail.com>
2025-04-13 17:06:02 +0900

    a

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_07_123151) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_13_074719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_07_123151) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "winner"
+    t.datetime "ended_at"
   end
 
   create_table "pieces", force: :cascade do |t|
